554e6296bb82c7149f5e1e18a047487b048c13c0
pjnalls <pjnalls.js@gmail.com>
2021-04-16 05:10:42 -0500

    Add `Employee` model for `DataTable` example.

---
+++ AspNetMvcEg/AspNetMvcEg/Models/Employee.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace AspNetMvcEg.Models
+{
+    public class Employee
+    {
+        public string EmployeeName { get; set; }
+        public string Company { get; set; }
+        public string Phone { get; set; }
+        public string Country { get; set; }
+        public string City { get; set; }
+        public string PostalCode { get; set; }
+    }
+}
