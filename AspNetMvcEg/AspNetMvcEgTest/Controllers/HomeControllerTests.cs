fb2ce83bc994d93b98d9a7c53aa52203ab6ed11b
pjnalls <pjnalls.js@gmail.com>
2021-05-15 04:56:00 -0500

    Generate `...Tests` proj. & write mini unit test for POC.

---
+++ AspNetMvcEg/AspNetMvcEgTest/Controllers/HomeControllerTests.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using AspNetMvcEg.Controllers;
+using System;
+using System.Web.Mvc;
+
+namespace AspNetMvcEgTest.Controllers
+{
+    [TestClass]
+    public class HomeControllerTests
+    {
+        [TestMethod]
+        public void TestLoadTableData()
+        {
+            var controller = new HomeController();
+            var result = controller.DataTable() as ViewResult;
+            Assert.AreEqual("", result.ViewName);
+        }
+    }
+}
---
+++ AspNetMvcEg/AspNetMvcEgTest/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("AspNetMvcEgTest")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("AspNetMvcEgTest")]
+[assembly: AssemblyCopyright("Copyright ©  2021")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("535908cc-44b9-44d1-8bff-7d1d8d31fb7a")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
